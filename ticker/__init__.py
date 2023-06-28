55c8d8428e31f8e88172564cf04454bd34ce8a0b
shreyanzjain <shreyanz32@gmail.com>
2023-06-29 01:10:24 +0530

    create models.py

--- ticker/__init__.py
+++ ticker/__init__.py
@@ -1,12 +1,4 @@
-from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
 from sqlalchemy import create_engine
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
--- ticker/routes.py
+++ ticker/routes.py
@@ -1,7 +1,8 @@
-from ticker import app, Item
+from ticker import app
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from ticker.models import Item
 
 templates = Jinja2Templates(directory="templates")
 
@@ -10,8 +11,11 @@ def read_root():
     return {"Hello": "world"}
 
 @app.get("/item/{item_id}", response_class=HTMLResponse)
-def read_item(request: Request, item_id: int, **kwargs):
-    return templates.TemplateResponse("item.html", {"request": request, "item_id": item_id})
+def read_item(request: Request, item_id: int, item_name: str):
+    return templates.TemplateResponse("item.html", 
+                                      {"request": request, 
+                                       "item_id": item_id, 
+                                       "item_name": item_name})
 
 @app.put("/item/{item_id}")
 def update_item(item_id: int, item: Item):
